30eded84238f04ebeba455208524c9d0bf0c28ac
Thiago Oliveira <thiagooac63@gmail.com>
2025-08-19 20:47:59 -0300

    feat: adicionar módulo de conexão MySQL (infra/conexao.js)

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import app from './src/app.js'
 
+import conexao from './infra/conexao.js'
+
 const port = 3000
 
-// Listening (Escutando)
-app.listen(port, () => {
-    console.log(`Servidor rodando \
-    em http://localhost:${port}`)
+// Estabelecer a conexão
+conexao.connect((error) => {
+    if (error) {
+        console.log("Erro na conexão:", error)
+    } else {
+        console.log("Conexão estabelecida com sucesso!")
+        // Listening (Escutando)
+        app.listen(port, () => {
+            console.log(`Servidor rodando em http://localhost:${port}`)
+        })
+    }
 })
