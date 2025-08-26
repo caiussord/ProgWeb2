0cac492fd29a2221c85c0e55e582f4c98f959a09
caiussord <caiussordillo@gmail.com>
2025-08-26 20:29:36 -0300

    atualização com CRUD

--- src/app.js
+++ src/app.js
@@ -1,97 +1,117 @@
 import express from 'express'
-
 import conexao from '../infra/conexao.js'
 
 const app = express()
 
 // Indicar para o express ler o body como json
-
 app.use(express.json())
 
-//const cursos = [
-//    {id: 1, disciplina: 'ADS'},
-//    {id: 2, disciplina: 'ADS'},
-//    {id: 3, disciplina: 'ADS'},
-//    {id: 4, disciplina: 'ADS'},
-//]
-
-function buscarCursosPorId(id){
-    return cursos.filter(curso => curso.id == id)
-}
-
-function buscarIndexCurso(id){
-    return cursos.findIndex(curso => curso.id == id)
-}
-
-// Criando uma rota default (endpoint) / req = request / res = response
-//app.get('/', (req, res)=> {
-//    res.send('Hello Thiago')
-//})
-
-// Rotas
-
-app.get('/cursos', (req, res)=> {
-//    res.status(200).send(cursos)
-    const sql = "SELECT * FROM cursos.curso;"
-    conexao.query(sql, (error, result) => {
-        if (error){
+app.post('/cursos', (req, res) => {
+    const { disciplina } = req.body
+    
+    if (!disciplina) {
+        return res.status(400).json({ error: 'Campo disciplina é obrigatório' })
+    }
+    
+    const sql = "INSERT INTO cursos.curso (disciplina) VALUES (?)"
+    conexao.query(sql, [disciplina], (error, result) => {
+        if (error) {
             console.log(error)
+            res.status(500).json({ error: 'Erro interno do servidor' })
+        } else {
+            res.status(201).json({ 
+                message: 'Curso criado', 
+                id: result.insertId,
+                disciplina: disciplina
+            })
+        }
+    })
+})
+
+app.get('/cursos', (req, res) => {
+    const sql = "SELECT * FROM cursos.curso"
+    conexao.query(sql, (error, result) => {
+        if (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Erro interno do servidor' })
         } else {
             res.status(200).json(result)
         }
     })
 })
 
-app.get('/cursos/:id', (req, res)=> {
-//    let index = req.params.id
-//    console.log(index)
-    res.json(buscarCursosPorId(req.params.id))
+app.get('/cursos/:id', (req, res) => {
+    const { id } = req.params
+    const sql = "SELECT * FROM cursos.curso WHERE id = ?"
+    
+    conexao.query(sql, [id], (error, result) => {
+        if (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Erro interno do servidor' })
+        } else if (result.length === 0) {
+            res.status(404).json({ error: 'Curso não encontrado' })
+        } else {
+            res.status(200).json(result[0])
+        }
+    })
 })
 
-//app.post('/cursos', (req, res)=> {
-//    cursos.push(req.body)
-//    res.status(200).send('Seleção cadastrada com sucesso!')
-//})
 
-app.post('/cursos', (req, res)=> {
-    const sql = "INSERT INTO cursos.curso (id, disciplina) VALUES (1, 'Prog. Web 2');"
-        conexao.query(sql, (error, result) => {
-            if (error){
-                console.log(error)
-            } else {
-                res.status(200).json(result)
-            }
-        })
-})
-
-//app.delete('/cursos/:id', (req, res)=> {
-//    let index = buscarIndexCurso(req.params.id)
-//    console.log(index)
-//    cursos.splice(index, 1)
-//    res.send(`O curso com id ${req.params.id} excluído com sucesso!`)
-//})
-
-app.delete('/cursos', (req, res)=> {
-    const sql = "DELETE FROM cursos.curso;"
-            conexao.query(sql, (error, result) => {
-                if (error){
-                    console.log(error)
-                } else {
-                    res.status(200).json(result)
-                }
+app.put('/cursos/:id', (req, res) => {
+    const { id } = req.params
+    const { disciplina } = req.body
+    
+    if (!disciplina) {
+        return res.status(400).json({ error: 'Campo disciplina é obrigatório' })
+    }
+    
+    const sql = "UPDATE cursos.curso SET disciplina = ? WHERE id = ?"
+    conexao.query(sql, [disciplina, id], (error, result) => {
+        if (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Erro interno do servidor' })
+        } else if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'Curso não encontrado' })
+        } else {
+            res.status(200).json({ 
+                message: 'Curso atualizado',
+                id: id,
+                disciplina: disciplina
             })
+        }
+    })
 })
 
-//app.put('/cursos/:id', (req, res)=> {
-//    let index = buscarIndexCurso(req.params.id)
-//    cursos[index].disciplina = req.body.disciplina
-//    res.json(cursos)
-//})
+app.delete('/cursos/:id', (req, res) => {
+    const { id } = req.params
+    const sql = "DELETE FROM cursos.curso WHERE id = ?"
+    
+    conexao.query(sql, [id], (error, result) => {
+        if (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Erro interno do servidor' })
+        } else if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'Curso não encontrado' })
+        } else {
+            res.status(200).json({ message: `Curso com id ${id} excluído` })
+        }
+    })
+})
 
-app.put('/cursos/:id', (req, res)=> {
-    let index = buscarIndexCurso(req.params.id)
-    cursos[index].disciplina = req.body.disciplina
-    res.json(cursos)
+
+app.delete('/cursos', (req, res) => {
+    const sql = "DELETE FROM cursos.curso"
+    conexao.query(sql, (error, result) => {
+        if (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Erro interno do servidor' })
+        } else {
+            res.status(200).json({ 
+                message: 'Todos os cursos foram excluídos',
+                deletedCount: result.affectedRows
+            })
+        }
+    })
 })
 
 export default app
